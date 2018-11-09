5365eb0ea6e1d5b560c12ba4a5813bf49f36d798
Ryan Lee <ryanjlee@berkeley.edu>
2018-11-08 22:36:42 -0800

    First

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="elem in commentary">
+        "{{elem.comment}}" - {{elem.by}}
+      </li>
+    </ul>
+    <div id="tilefish_appreciation">
+      Show your support for the tilefish by SMASHING this button!!!!
+      <br>
+      <button class="btn btn-primary" v-on:click="counter++">Click to donate $5 to the WTF (World Wide Tilefish Foundation &copy;)</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      commentary: [
+        {comment: "This fish is rad", by: "Max Weber"},
+        {comment: "I wish more people knew about this amazing fish", by: "Barack Obama"},
+        {comment: "I didn't know that these fish <FILL IN FUN FACT>!", by: "Johnny Cash"},
+        {comment: "Tilefish are cooler than Guppies!!!!", by: "Munis Thahir"},
+      ],
+      counter: 0,
+      msg: 'The Tilefish.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  width: 100vw;
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
